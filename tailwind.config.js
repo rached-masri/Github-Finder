6466265b422ee17726e79dfbbb37225307a9ed31
Rached Masri <masri.rached@gmail.com>
2022-03-01 10:54:05 +0100

    Create Navbar Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [require('daisyui')],
