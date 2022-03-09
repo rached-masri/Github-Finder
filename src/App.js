67f55b0667abc73cd03ba2355e9f54c74adeb29c
Rached Masri <masri.rached@gmail.com>
2022-03-09 18:05:58 +0100

    Add Alert Context & Reducer

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
       <Router>
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
@@ -22,6 +24,7 @@ function App() {
         </main>
         <Footer />
       </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
